c40801f61e48be0bf8e11994e88c141b309b750f
Nicholas Massenkoff <nmassenkoff@gmail.com>
2013-10-03 00:04:15 -0700

    done for the night

---
+++ cs110c/programs/modTest.cpp
@@ -0,0 +1,39 @@
+// =====================================================================================
+// 
+//       Filename:  modTest.cpp
+// 
+//    Description:  
+// 
+//        Version:  1.0
+//        Created:  10/02/2013 13:12:58
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  Nicholas Massenkoff (M4551V3), nmassenkoff@gmail.com
+//        Company:  
+// 
+// =====================================================================================
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char *argv[])
+{
+	cout << 0 % 4 << endl;
+	cout << 1 % 4 << endl;
+	cout << 2 % 4 << endl;
+	cout << 3 % 4 << endl;
+	cout << 4 % 4 << endl;
+	cout << 15 % 20 << endl;
+	cout << 16 % 20 << endl;
+	cout << 17 % 20 << endl;
+	cout << 20 % 20 << endl;
+	cout << 40 % 20 << endl;
+	cout << 41 % 20 << endl;
+	cout << 23 % 100 << endl;
+	cout << 3 % 100 << endl;
+	cout << 100 % 100 << endl;
+
+	return 0;
+}

ac0a3c9cb6be3f36be7903917dd8c69554c06cb1
Nicholas Massenkoff <nmassenkoff@gmail.com>
2013-10-02 21:27:07 -0700

    Added larger spacing to numbers as wheel animates

--- cs110c/programs/lab_2/Wheel.cpp
+++ cs110c/programs/lab_2/Wheel.cpp
@@ -38,11 +38,12 @@ Wheel::Wheel(): List(), currentPos(1)
 
 	// Randomly select each possible item and add them to the wheel list
 	int random_pos;
-	int value;
+	ListItemType value;
 	for (int i = 1; i <= 20; i++)
 	{
 		random_pos = randomInt(1,possibleValues.getLength());
 		possibleValues.retrieve(random_pos, value);
+
 		push(value);
 		possibleValues.remove(random_pos);
 	}
@@ -78,6 +79,136 @@ int Wheel::translatePos(int linearPos) const
 	return ((linearPos -1) % 20) + 1;
 }
 
+void printFirstNum(int offset, int value)
+{
+	// Helper function for displaySpin() that determines which digits
+	// of the first number to print
+	// pre: 0 <= offset <= 3; value representing whatever is currently the first
+	//      number displayed on the wheel.
+	// post: none
+	
+	int dec = value % 100;
+	if (dec >= 10)
+	{
+		// value is 2 digits
+		switch(offset)
+		{
+			case 0:
+				cout << ' ' << value << ' ';
+				break;
+			case 1:
+				cout << value << ' ';
+				break;
+			case 2:
+				cout << value % 10 << ' ';
+				break;
+			case 3:
+				cout << ' ';
+				break;
+		}
+	}
+	else if (dec >= 1)
+	{
+		// value is 1 digit
+		switch(offset)
+		{
+			case 0:
+				cout << ' ' << value << "  ";
+				break;
+			case 1:
+				cout << value << "  ";
+				break;
+			case 2:
+				cout << "  ";
+				break;
+			case 3:
+				cout << ' ';
+				break;
+		}
+
+	}
+	else 
+		// value is 100
+		switch(offset)
+		{
+			case 0:
+				cout << value << ' ';
+				break;
+			case 1:
+				cout << "00 ";
+				break;
+			case 2:
+				cout << "0 ";
+				break;
+			case 3:
+				cout << " ";
+				break;
+		}
+}
+void printLastNum(int offset, int value)
+{
+	// Helper funciton for displaySpin() that determines which digits of the
+	// last number to print
+	// pre: 0 <= offset <= 3; value representing whatever is currently the last
+	//      number displayed on the wheel.
+	// post: none
+
+	int dec = value % 100;
+	if (dec >= 10)
+	{
+		// value is 2 digits
+		switch(offset)
+		{
+			case 0:
+				break;
+			case 1:
+				cout << ' ';
+				break;
+			case 2:
+				cout << ' ' << value / 10;
+				break;
+			case 3:
+				cout << ' ' << value;
+				break;
+		}
+	}
+	else if (dec >= 1)
+	{
+		// value is 1 digit
+		switch(offset)
+		{
+			case 0:
+				break;
+			case 1:
+				cout << " ";
+				break;
+			case 2:
+				cout << ' ' << value;
+				break;
+			case 3:
+				cout << ' ' << value << ' ';
+				break;
+		}
+
+	}
+	else 
+		// value is 100
+		switch(offset)
+		{
+			case 0:
+				break;
+			case 1:
+				cout << "1";
+				break;
+			case 2:
+				cout << "10";
+				break;
+			case 3:
+				cout << "100";
+				break;
+		}
+}
+
 void Wheel::displaySpin(int targetPos) const
 {
 	// initialize variables pertaining to the wheel spin physics
@@ -87,77 +218,58 @@ void Wheel::displaySpin(int targetPos) const
 	bool preSpin = true;
 
 	// Print wheel arrow that points at current value
-	cout << "                           |\n";
-	cout << "                           |\n";
-	cout << "                           |\n";
-	cout << "                           V\n";
+	cout << "                                    |\n";
+	cout << "                                    |\n";
+	cout << "                                    |\n";
+	cout << "                                    V\n";
 
 	// Initialize variables that help determine how to wrap numbers arround
 	int i, j;
-	int pos = 1;
+	int offset = 1;
 	int numItems = getLength();
 	int tempPos = currentPos;
 	int lastItem = translatePos(currentPos + 10);
 	int firstItem = translatePos(currentPos + 11);
 	ListItemType listItemValue;
+	
 	while(true)
 	{
 		// Main loop that animates the wheel spinning
-		j = 2;
-		if ( pos == 1 )
-		{
-			// Position one prints the last digit of the first value 
-			retrieve(firstItem, listItemValue);
-			cout << listItemValue % 10;
-			cout << ' ';
-		}
-		else if ( pos == 0)
-		{
-			// Position 0 prints the whole first value
-			retrieve(firstItem, listItemValue);
-			cout << listItemValue;
-			cout << ' ';
-		}
 
+		// Print first number accounting for the offset position
+		retrieve(firstItem, listItemValue);
+		printFirstNum(offset, listItemValue);
+
+		j = 2; // skip 1st item of wheel
 		for (i = firstItem + 1; j <= numItems ; i++) {
 			// This loop prints all the values that aren't the first value
-			// and depending on the position, a space before the value or after
-			if (pos == 2)
-			{
-				retrieve(translatePos(i), listItemValue);
-				cout << ' ' << listItemValue;
-			}
+			// and depending on the position and number of digits, spaces
+			// before the value or after
+
+			retrieve(translatePos(i), listItemValue);
+			if (listItemValue % 100 >= 10)
+				cout << ' ' << listItemValue << ' ';
+			else if (listItemValue % 100 >= 1)
+				cout << ' ' << listItemValue << "  ";
 			else
-			{
-				retrieve(translatePos(i), listItemValue);
-				cout << listItemValue << ' ';	
-			}
+				cout << listItemValue << ' ';
+
 			j++;
 		}
 
 		// Determine what to print at the end of the line depending on the position
-		// Also advance tempPos if position has cycled to next position
-		if ( pos == 1) 
-		{
-			retrieve(firstItem, listItemValue);
-			cout << "\b ";
-			cout << listItemValue / 10;
-			cout << ' ';
-			pos++;
+		retrieve(firstItem, listItemValue);
+		printLastNum(offset, listItemValue);
 
-		} else if ( pos == 2) {
-			retrieve(firstItem, listItemValue);
-			cout << " ";
-			cout << listItemValue;
-			cout << ' ';
-			pos = 0;
+		// Cycle offset counter
+		offset = (offset + 1) % 3;
+		if (offset == 0) 
+		{
+			// Advance tempPos if position has cycled through current number
 			tempPos = translatePos(tempPos + 1);
 			firstItem = translatePos(tempPos + 11);
-
-		} else {
-			cout << ' ';
-			pos++;
 		}
+
 
 		// Delay the update of the display to simulate spinning animation
 		// Flush output buffer and return cursor to begining of line to print 
@@ -168,7 +280,7 @@ void Wheel::displaySpin(int targetPos) const
 
 		if (preSpin)
 		{
-			// If intial frame of spin, pause first to
+			// If initial frame of spin, pause first to
 			// show current position of values before animating spin
 			usleep(1000000);
 			preSpin = false;
@@ -178,9 +290,9 @@ void Wheel::displaySpin(int targetPos) const
 		delay = resistance * (pow(speed,17));
 		speed+= 0.1;
 		
-		// If spin slows enough and current pos matches target position then stop
+		// If spin slows enough and current offset matches target position then stop
 		// animation and exit function
-		if (delay > 80000 && tempPos == targetPos && pos == 2)
+		if (delay > 80000 && tempPos == targetPos && offset == 2)
 		{
 			cout << endl;
 			break;
